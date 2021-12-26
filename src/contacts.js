eafe0a7098aadeb1bf4c89fe3aae537c42c81841
jsonsfather <94735082+jsonsfather@users.noreply.github.com>
2021-12-26 17:36:41 +0330

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,20 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Esfahan1 ",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Naghshejahan.jpg/1024px-Naghshejahan.jpg",
+
+    email: "hadihalvani@gmail.com"
+  },
+  {
+    id: 2,
+    name: "esfahan2",
+    imgURL:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.archdaily.com%2F920829%2Firanian-architects-announce-first-inaugural-isfahan-prize%2F5d26196e284dd1b4460000d9-iranian-architects-announce-first-inaugural-isfahan-prize-image&psig=AOvVaw1-26Xl3qfcDTDXHmlvef8T&ust=1640610911296000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDTzMLGgfUCFQAAAAAdAAAAABAI",
+
+    email: "hadi2040@gmail.com"
+  }
+];
+
+export default contacts;
